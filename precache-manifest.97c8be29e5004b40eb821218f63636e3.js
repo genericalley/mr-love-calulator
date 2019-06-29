f9c40aebb1daf7239da8a01fad2f89fc7e12fedc
genericalley <kristen.pietsch@gmail.com>
2019-06-29 03:35:17 -0500

    Updates

---
+++ precache-manifest.97c8be29e5004b40eb821218f63636e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d48001873ccede64f3848a5ef37e89c1",
+    "url": "/mr-love-calulator/index.html"
+  },
+  {
+    "revision": "89b00e6879b952c59982",
+    "url": "/mr-love-calulator/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "25f745ef2376eb1f1a22",
+    "url": "/mr-love-calulator/static/js/2.3a0fec63.chunk.js"
+  },
+  {
+    "revision": "89b00e6879b952c59982",
+    "url": "/mr-love-calulator/static/js/main.352a8f36.chunk.js"
+  },
+  {
+    "revision": "8eac044eb989ebfe6509",
+    "url": "/mr-love-calulator/static/js/runtime~main.90ccea8b.js"
+  }
+]);
